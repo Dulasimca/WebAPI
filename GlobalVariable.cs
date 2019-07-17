945ce234a4225551ac7b108a03c9102ae89fe843
Dulasimca <dulasimca@gmail.com>
2019-07-17 11:42:28 +0530

    Globalvariable changes

--- GlobalVariable.cs
+++ GlobalVariable.cs
@@ -8,17 +8,18 @@ namespace TNCSCAPI
     public class GlobalVariable
     {
         //Testing connection
-        //public const string ConnectionString = "data source=localhost;initial catalog=TNCSCSCM;user id = sqladmin; password =sql@svc&ac!72;";
-        //public const string ReportPath = "C://LocalRepository//TNCSCUI//dist//GoodsStock//assets//";
+        public const string ConnectionString = "data source=localhost;initial catalog=TNCSCSCM;user id = sqladmin; password =sql@svc&ac!72;";
+        public const string ReportPath = "C://LocalRepository//TNCSCUI//dist//GoodsStock//assets//";
 
         //Live connection
-        public const string ConnectionString = "data source=localhost;initial catalog=TNCSCSCM;user id = sqladmin; password =sql@svc&ac!34;";
-        public const string ReportPath = "C://Repos//LiveCode//TNCSCWebSite//assets//";
+        //public const string ConnectionString = "data source=localhost;initial catalog=TNCSCSCM;user id = sqladmin; password =sql@svc&ac!34;";
+        //public const string ReportPath = "C://Repos//LiveCode//TNCSCWebSite//assets//";
 
         public const string StockDORegisterFileName = "DOREG";
         public const string StockTruckMemoRegisterFileName = "TMREG";
         public const string StockReceiptRegisterFileName = "REREG";
         public const string StockIssueRegisterFileName = "ISREG";
+        public const string StockReceiptFileName = "REDOC";
 
     }  
 }
