546b2e9df2f9ca2a71748a859a93f1a33bf32b14
Dulasimca <dulasimca@gmail.com>
2019-07-26 18:07:13 +0530

    Added Basic weight

---
+++ Controllers/Masters/BasicweightController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Data;
+
+namespace TNCSCAPI.Controllers.Masters
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BasicweightController : ControllerBase
+    {
+
+        [HttpGet]
+        public string Get()
+        {
+            DataSet ds = new DataSet();
+            ManageSQLConnection manageSQLConnection = new ManageSQLConnection();
+            ds = manageSQLConnection.GetDataSetValues("GetBasicWeightMaster");
+            return JsonConvert.SerializeObject(ds.Tables[0]);
+        }
+    }
+}
