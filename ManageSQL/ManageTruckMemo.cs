a7871329787f20004cafb84e79d7e44a81858d9c
Dulasimca <dulasimca@gmail.com>
2019-07-30 12:37:12 +0530

    Truck memochanges

--- ManageSQL/ManageTruckMemo.cs
+++ ManageSQL/ManageTruckMemo.cs
@@ -50,10 +50,10 @@ namespace TNCSCAPI.ManageSQL
                     sqlCommand.Parameters.AddWithValue("@GunnyUtilised", documentStockTransferDetails.GunnyUtilised);
                     sqlCommand.Parameters.AddWithValue("@GunnyReleased", documentStockTransferDetails.GunnyReleased);
                     sqlCommand.Parameters.AddWithValue("@IssueSlip", documentStockTransferDetails.IssueSlip);
-                    sqlCommand.Parameters.AddWithValue("@ExportFlag", documentStockTransferDetails.ExportFlag);
-                    sqlCommand.Parameters.AddWithValue("@Flag1", documentStockTransferDetails.Flag1);
-                    sqlCommand.Parameters.AddWithValue("@Flag2", documentStockTransferDetails.Flag2);
-                    sqlCommand.Parameters.AddWithValue("@TruckMemo", documentStockTransferDetails.TruckMemo);
+                    //sqlCommand.Parameters.AddWithValue("@ExportFlag", "N");
+                    //sqlCommand.Parameters.AddWithValue("@Flag1", documentStockTransferDetails.Flag1);
+                    //sqlCommand.Parameters.AddWithValue("@Flag2", documentStockTransferDetails.Flag2);
+                    //sqlCommand.Parameters.AddWithValue("@TruckMemo", documentStockTransferDetails.TruckMemo);
                     sqlCommand.Parameters.AddWithValue("@STNo1", documentStockTransferDetails.STNo);
                     sqlCommand.Parameters.AddWithValue("@RowId1", documentStockTransferDetails.RowId);
                     sqlCommand.Parameters.Add("@STNo", SqlDbType.NVarChar, 10);
