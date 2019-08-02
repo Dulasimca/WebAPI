34123b03ebfe23a6d8dc22c7917a98a37504793c
Dulasimca <dulasimca@gmail.com>
2019-08-02 15:46:17 +0530

    Truck memo changes

--- ManageAllReports/Document/ManageDocumentTruckMemo.cs
+++ ManageAllReports/Document/ManageDocumentTruckMemo.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 using TNCSCAPI.Models.Documents;
 
 namespace TNCSCAPI.ManageAllReports.Document
@@ -18,7 +15,7 @@ namespace TNCSCAPI.ManageAllReports.Document
             bool isDuplicate = false;
             try
             {
-                fileName = truckMemoList.ReceivingCode + GlobalVariable.DocumentReceiptFileName;
+                fileName = truckMemoList.IssuingCode + GlobalVariable.DocumentTruckMemoFileName;
                 fPath = GlobalVariable.ReportPath + "Reports";
                 report.CreateFolderIfnotExists(fPath); // create a new folder if not exists
                 subF_Path = fPath + "//" + truckMemoList.UserID; //ManageReport.GetDateForFolder();
@@ -58,36 +55,36 @@ namespace TNCSCAPI.ManageAllReports.Document
             streamWriter.WriteLine("|                                                                                                          |");
             streamWriter.WriteLine("|                                                                                                          |");
             streamWriter.WriteLine("|                                       TAMILNADU CIVIL SUPPLIES CORPORATION                               |");
-            streamWriter.WriteLine("|                                           ");
+            streamWriter.Write("|                                           ");
             streamWriter.Write(report.StringFormatWithoutPipe("REGION : ", 9, 1));
-            streamWriter.Write(report.StringFormat(truckMemoList.RegionName, 55, 2));
+            streamWriter.Write(report.StringFormat(truckMemoList.RegionName, 53, 2));
             streamWriter.WriteLine("");
             streamWriter.WriteLine("|                                                                                                          |");
             streamWriter.WriteLine("|                                                                                                          |");
-            if (!isDuplicate)
+            if (isDuplicate)
             {
-                streamWriter.WriteLine("|                                      STOCK ISSUE TRUCK MEMO            DUPLICATE COPY          |");
+                streamWriter.WriteLine("|                                      STOCK ISSUE TRUCK MEMO            DUPLICATE COPY                    |");
             }
             else
             {
-                streamWriter.WriteLine("|                                      STOCK ISSUE TRUCK MEMO                                    |");
+                streamWriter.WriteLine("|                                      STOCK ISSUE TRUCK MEMO                                              |");
             }
             streamWriter.WriteLine("|----------------------------------------------------------------------------------------------------------|");
             streamWriter.Write("|TRUCK MEMO NO:");
             streamWriter.Write(report.StringFormatWithoutPipe(truckMemoList.STNo, 21, 2));
             streamWriter.Write("ALLOTMENT/RELEASE ORDER: ");
-            streamWriter.Write(report.StringFormatWithoutPipe(truckMemoList.MNo, 12, 2));
+            streamWriter.Write(report.StringFormatWithoutPipe(truckMemoList.RNo, 12, 2));
             streamWriter.Write("MOVE. ORDER NO: ");
-            streamWriter.Write(report.StringFormatWithoutPipe(report.FormatDate(truckMemoList.MDate.ToString()), 16, 2));
+            streamWriter.Write(report.StringFormatWithoutPipe(truckMemoList.MNo, 15, 2));
             streamWriter.Write("|");
             streamWriter.WriteLine(" ");
 
             streamWriter.Write("|              DATE:");
-            streamWriter.Write(report.StringFormatWithoutPipe(report.FormatDate(truckMemoList.RDate.ToString()), 21, 2));
+            streamWriter.Write(report.StringFormatWithoutPipe(report.FormatDate(truckMemoList.STDate.ToString()), 21, 2));
             streamWriter.Write(report.StringFormatWithoutPipe("TIME: ", 25, 1));
-            streamWriter.Write(report.StringFormatWithoutPipe("", 41, 2));
+            streamWriter.Write(report.StringFormatWithoutPipe("", 16, 2));
             streamWriter.Write("DATE:");
-            streamWriter.Write(report.StringFormatWithoutPipe(report.FormatDate(truckMemoList.RDate.ToString()), 21, 2));
+            streamWriter.Write(report.StringFormatWithoutPipe(report.FormatDate(truckMemoList.RDate.ToString()), 16, 2));
             streamWriter.Write("|");
             streamWriter.WriteLine(" ");
 
@@ -98,11 +95,11 @@ namespace TNCSCAPI.ManageAllReports.Document
             streamWriter.Write("|");
             streamWriter.WriteLine(" ");
 
-            streamWriter.WriteLine("|-----------------------------------------------------------------------------------------------------------|");
-            streamWriter.WriteLine("||---------------------------------------------------------------------------------------------------------||");
-            streamWriter.WriteLine("||SNo |STACK NO   |COMMODITY           | SCHEME       |UNIT WEIGHT  |NO.OF |  Gross        NET   |% OF     ||");
-            streamWriter.WriteLine("||    |           |                    |              |             |  UNIT|   WEIGHT in Kgs/NOs |MOISTURE ||");
-            streamWriter.WriteLine("||---------------------------------------------------------------------------------------------------------||");
+            streamWriter.WriteLine("|----------------------------------------------------------------------------------------------------------|");
+            streamWriter.WriteLine("||--------------------------------------------------------------------------------------------------------||");
+            streamWriter.WriteLine("||SNo |STACK NO   |COMMODITY           | SCHEME       |UNIT WEIGHT  |NO.OF |  Gross        NET   |% OF    ||");
+            streamWriter.WriteLine("||    |           |                    |              |             |  UNIT|   WEIGHT in Kgs/NOs |MOISTURE||");
+            streamWriter.WriteLine("||--------------------------------------------------------------------------------------------------------||");
         }
 
         /// <summary>
@@ -118,14 +115,14 @@ namespace TNCSCAPI.ManageAllReports.Document
                 i = i + 1;
                 streamWriter.Write("||");
                 streamWriter.Write(report.StringFormat(i.ToString(), 4, 2));
-                streamWriter.Write(report.StringFormat(i.ToString(), 11, 2));
-                streamWriter.Write(report.StringFormat(i.ToString(), 20, 2));
-                streamWriter.Write(report.StringFormat(i.ToString(), 14, 2));
-                streamWriter.Write(report.StringFormat(i.ToString(), 13, 2));
-                streamWriter.Write(report.StringFormat(i.ToString(), 6, 1));
-                streamWriter.Write(report.StringFormat(i.ToString(), 10, 1));
-                streamWriter.Write(report.StringFormat(i.ToString(), 10, 1));
-                streamWriter.Write(report.StringFormat(i.ToString(), 8, 1));
+                streamWriter.Write(report.StringFormat(item.TStockNo, 11, 2));
+                streamWriter.Write(report.StringFormat(item.ITDescription, 20, 2));
+                streamWriter.Write(report.StringFormat(item.SchemeName, 14, 2));
+                streamWriter.Write(report.StringFormat(item.PackingType, 13, 2));
+                streamWriter.Write(report.StringFormat(item.NoPacking.ToString(), 6, 1));
+                streamWriter.Write(report.StringFormat(item.GKgs.ToString(), 10, 1));
+                streamWriter.Write(report.StringFormat(item.Nkgs.ToString(), 10, 1));
+                streamWriter.Write(report.StringFormat(item.Moisture.ToString(), 8, 1) + "|");
                 streamWriter.WriteLine(" ");
             }
         }
@@ -137,33 +134,74 @@ namespace TNCSCAPI.ManageAllReports.Document
         /// <param name="truckMemoList"></param>
         public void AddFooter(StreamWriter streamWriter, DocumentStockTransferDetails transferDetails)
         {
-            streamWriter.WriteLine("|-----------------------------------------------------------------------------------------------------------|");
+            streamWriter.WriteLine("|----------------------------------------------------------------------------------------------------------|");
             streamWriter.Write("|LORRY NO: ");
-            streamWriter.Write(report.StringFormatWithoutPipe(transferDetails.LorryNo, 13, 2));
-            streamWriter.Write("TC NAME      : ");
-            streamWriter.Write(report.StringFormatWithoutPipe(transferDetails.TransactionName, 14, 2));
+            streamWriter.Write(report.StringFormatWithoutPipe(transferDetails.LorryNo, 23, 2));
+            streamWriter.Write("TC NAME   : ");
+            streamWriter.Write(report.StringFormatWithoutPipe(transferDetails.TransactionName, 59, 2) + "|");
+            streamWriter.WriteLine(" ");
             streamWriter.Write("|MODE OF WEIGHMENT : ");
-            streamWriter.Write(report.StringFormatWithoutPipe("", 28, 2));
+            streamWriter.Write(report.StringFormatWithoutPipe(GetWTCode(transferDetails), 85, 2));
             streamWriter.Write("|");
             streamWriter.WriteLine(" ");
             streamWriter.Write("|FROM RAIL HEAD: ");
-            streamWriter.Write(report.StringFormatWithoutPipe("-", 13, 2));
-            streamWriter.Write("WAGON NO : ");
-            streamWriter.Write(report.StringFormatWithoutPipe("-", 59, 2));
+            streamWriter.Write(report.StringFormatWithoutPipe(transferDetails.RailHeadName, 17, 2));
+            streamWriter.Write("WAGON NO  : ");
+            streamWriter.Write(report.StringFormatWithoutPipe(GetWNo(transferDetails), 21, 2));
             streamWriter.Write("RR NO:");
-            streamWriter.WriteLine(report.StringFormatWithoutPipe("-", 59, 2));
+            streamWriter.Write(report.StringFormatWithoutPipe(GetRRNo(transferDetails), 31, 2));
             streamWriter.Write("|");
-            streamWriter.Write(" ");
-            streamWriter.WriteLine("|                                                                                                           |");
-            streamWriter.WriteLine("|The above stocks were weighed in our presence Received in Good Conditions and taken into account           |");
-            streamWriter.WriteLine("|                                                                                                           |");
-            streamWriter.WriteLine("|                                                                       NAME OF TC                          |");
-            streamWriter.WriteLine("|                                                                                                           |");
-            streamWriter.WriteLine("|GODOWN INCHARGE                                                        SIGNATURE OF THE TC REPRESENTATIVE  |");
-            streamWriter.WriteLine("|REMARKS                                                                                                    |");
-            streamWriter.WriteLine("|                                                                                                           |");
-            streamWriter.WriteLine("|-----------------------------------------------------------------------------------------------------------|");
-            streamWriter.WriteLine((char)12);   
+            streamWriter.WriteLine(" ");
+            streamWriter.WriteLine("|                                                                                                          |");
+            streamWriter.WriteLine("|The above stocks were weighed in our presence Received in Good Conditions and taken into account          |");
+            streamWriter.WriteLine("|                                                                                                          |");
+            streamWriter.WriteLine("|                                                                       NAME OF TC                         |");
+            streamWriter.WriteLine("|                                                                                                          |");
+            streamWriter.WriteLine("|GODOWN INCHARGE                                                        SIGNATURE OF THE TC REPRESENTATIVE |");
+            streamWriter.WriteLine("|REMARKS                                                                                                   |");
+            streamWriter.WriteLine("|                                                                                                          |");
+            streamWriter.WriteLine("|----------------------------------------------------------------------------------------------------------|");
+            streamWriter.WriteLine((char)12);
+        }
+
+        public string GetWTCode(DocumentStockTransferDetails transferDetails)
+        {
+            string WtCode = string.Empty;
+            try
+            {
+                WtCode = transferDetails.documentSTItemDetails[0].WTCode;
+            }
+            catch
+            {
+            }
+            return WtCode;
+        }
+
+        public string GetWNo(DocumentStockTransferDetails transferDetails)
+        {
+            string WtCode = string.Empty;
+            try
+            {
+                WtCode = transferDetails.documentSTTDetails[0].Wno;
+            }
+            catch
+            {
+            }
+            return WtCode;
+        }
+
+
+        public string GetRRNo(DocumentStockTransferDetails transferDetails)
+        {
+            string WtCode = string.Empty;
+            try
+            {
+                WtCode = transferDetails.documentSTTDetails[0].RRNo;
+            }
+            catch
+            {
+            }
+            return WtCode;
         }
     }
 }
