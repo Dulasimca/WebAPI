8b33f35f063ada3cf6ba5c6a8f41c8d07ee80878
subashvishal <43237843+subashvishal@users.noreply.github.com>
2019-08-22 22:52:00 +0530

    Society Master Entry included

---
+++ Controllers/Masters/SocietyMasterEntryController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using TNCSCAPI.ManageSQL;
+
+namespace TNCSCAPI.Controllers.Masters
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SocietyMasterEntryController
+    {
+        [HttpGet("{id}")]
+        public string Get(string GCode)
+        {
+            DataSet ds = new DataSet();
+            ManageSQLConnection manageSQLConnection = new ManageSQLConnection();
+            List<KeyValuePair<string, string>> sqlParameters = new List<KeyValuePair<string, string>>();
+            sqlParameters.Add(new KeyValuePair<string, string>("@gcode", GCode));
+            ds = manageSQLConnection.GetDataSetValues("get_societymasterentryissuer", sqlParameters);
+            return JsonConvert.SerializeObject(ds.Tables[0]);
+        }
+    }
+
+    public class SocietyMasterEntryIssuerEntity
+    {
+        public string Tyname { get; set; }
+        public string Societyname { get; set; }
+        public string Issuername { get; set; }
+    }
+}
