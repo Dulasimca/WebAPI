054e5577090d104b373db813883cbf12a649bb04
Your Name <subashvishal64@gmail.com>
2019-08-24 15:30:51 +0530

    Society Master New Entry Controller included

--- Controllers/Masters/SocietyMasterEntryController.cs
+++ Controllers/Masters/SocietyMasterEntryController.cs
@@ -21,10 +21,26 @@ namespace TNCSCAPI.Controllers.Masters
             ds = manageSQLConnection.GetDataSetValues("get_societymasterentryissuer", sqlParameters);
             return JsonConvert.SerializeObject(ds.Tables[0]);
         }
+        [HttpPut("{id}")]
+        public bool Put(SocietyMasterEntryIssuerEntity societyMasterEntry)
+        {
+            DataSet ds = new DataSet();
+            ManageSQLConnection manageSQLConnection = new ManageSQLConnection();
+            List<KeyValuePair<string, string>> sqlParameters = new List<KeyValuePair<string, string>>();
+            sqlParameters.Add(new KeyValuePair<string, string>("@soccode", societyMasterEntry.SCode));
+            sqlParameters.Add(new KeyValuePair<string, string>("@icode", societyMasterEntry.ICode));
+            sqlParameters.Add(new KeyValuePair<string, string>("@itype", societyMasterEntry.IType));
+            sqlParameters.Add(new KeyValuePair<string, string>("@gcode", societyMasterEntry.GCode));
+            return manageSQLConnection.UpdateValues("update_societymasterentry", sqlParameters);
+        }
     }
 
     public class SocietyMasterEntryIssuerEntity
     {
+        public string SCode { get; set; }
+        public string ICode { get; set; }
+        public string IType { get; set; }
+        public string GCode { get; set; }
         public string Tyname { get; set; }
         public string Societyname { get; set; }
         public string Issuername { get; set; }
---
+++ Controllers/Masters/SocietyMasterNewController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using TNCSCAPI.ManageSQL;
+
+namespace TNCSCAPI.Controllers.Masters
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SocietyMasterNewController
+    {
+        [HttpGet("{id}")]
+        public string Get(string GCode)
+        {
+            DataSet ds = new DataSet();
+            ManageSQLConnection manageSQLConnection = new ManageSQLConnection();
+            List<KeyValuePair<string, string>> sqlParameters = new List<KeyValuePair<string, string>>();
+            sqlParameters.Add(new KeyValuePair<string, string>("@gcode", GCode));
+            ds = manageSQLConnection.GetDataSetValues("[getsocietymasterentry]", sqlParameters);
+            return JsonConvert.SerializeObject(ds.Tables[0]);
+        }
+        [HttpPost("{id}")]
+        public bool Post(SocietyMasterNewEntity societyMasterNewEntry)
+        {
+            //ManageEmployeeDetailsSQL manageEmployee = new ManageEmployeeDetailsSQL();
+            List<KeyValuePair<string, string>> sqlParameters = new List<KeyValuePair<string, string>>();
+            ManageSQLConnection manageSQL = new ManageSQLConnection();
+            sqlParameters.Add(new KeyValuePair<string, string>("@soccode", societyMasterNewEntry.SocietyCode));
+            sqlParameters.Add(new KeyValuePair<string, string>("@socname", societyMasterNewEntry.SocietyName));
+            sqlParameters.Add(new KeyValuePair<string, string>("@soctype", societyMasterNewEntry.SocietyType));
+            sqlParameters.Add(new KeyValuePair<string, string>("@godowncode", societyMasterNewEntry.gowdoncode));
+            sqlParameters.Add(new KeyValuePair<string, string>("@RCode", societyMasterNewEntry.RCode)); 
+            sqlParameters.Add(new KeyValuePair<string, string>("@eflag", societyMasterNewEntry.eflag));
+            return manageSQL.InsertData("Insertsocietymasterentry", sqlParameters);
+        }
+    }
+
+    public class SocietyMasterNewEntity
+    {
+        public string SocietyCode { get; set; }
+        public string SocietyName { get; set; }
+        public string SocietyType { get; set; }
+        public string gowdoncode { get; set; }
+        public string RCode{ get; set; }
+        public string eflag { get; set; }
+    }
+}
