036db831055311b3ca0ebc9f127f0f622d31f214
Dulasimca <dulasimca@gmail.com>
2019-10-09 12:05:47 +0530

    Changes Added for sales report

--- Controllers/Reports/Sales/SalesController.cs
+++ Controllers/Reports/Sales/SalesController.cs
@@ -19,27 +19,40 @@ namespace TNCSCAPI.Controllers.Reports.Sales
         {
             DataSet ds = new DataSet();
             ManageSQLConnection manageSQLConnection = new ManageSQLConnection();
-            List<KeyValuePair<string, string>> sqlParameters = new List<KeyValuePair<string, string>>();
-            sqlParameters.Add(new KeyValuePair<string, string>("@Godcode", issueMemoDetails.GCode));
-            sqlParameters.Add(new KeyValuePair<string, string>("@society", issueMemoDetails.SCode));
-            sqlParameters.Add(new KeyValuePair<string, string>("@type", issueMemoDetails.TCode));
-            sqlParameters.Add(new KeyValuePair<string, string>("@FDATE", issueMemoDetails.Fdate));
-            sqlParameters.Add(new KeyValuePair<string, string>("@ToDate", issueMemoDetails.Tdate));
-            ds = manageSQLConnection.GetDataSetValues("Getissuememo", sqlParameters);
+            if (issueMemoDetails.Type == 1)
+            {
+                List<KeyValuePair<string, string>> sqlParameters = new List<KeyValuePair<string, string>>();
+                sqlParameters.Add(new KeyValuePair<string, string>("@Godcode", issueMemoDetails.GCode));
+                sqlParameters.Add(new KeyValuePair<string, string>("@society", issueMemoDetails.SCode));
+                sqlParameters.Add(new KeyValuePair<string, string>("@type", issueMemoDetails.TCode));
+                sqlParameters.Add(new KeyValuePair<string, string>("@FDATE", issueMemoDetails.Fdate));
+                sqlParameters.Add(new KeyValuePair<string, string>("@ToDate", issueMemoDetails.Tdate));
+                ds = manageSQLConnection.GetDataSetValues("Getissuememo", sqlParameters);
+            }
+            else if (issueMemoDetails.Type == 2)
+            {
+                
+                List<KeyValuePair<string, string>> sqlParameters = new List<KeyValuePair<string, string>>();
+                sqlParameters.Add(new KeyValuePair<string, string>("@FDATE", issueMemoDetails.Fdate));
+                sqlParameters.Add(new KeyValuePair<string, string>("@ToDate", issueMemoDetails.Tdate));
+                sqlParameters.Add(new KeyValuePair<string, string>("@Godcode", issueMemoDetails.GCode));
+                ds = manageSQLConnection.GetDataSetValues("getIssuememoabstract", sqlParameters);
+                
+            }
             return JsonConvert.SerializeObject(ds.Tables[0]);
         }
-        [HttpGet("{id}")]
-        public string Get(string Fdate, string Tdate, string GCode)
-        {
-            DataSet ds = new DataSet();
-            ManageSQLConnection manageSQLConnection = new ManageSQLConnection();
-            List<KeyValuePair<string, string>> sqlParameters = new List<KeyValuePair<string, string>>();
-            sqlParameters.Add(new KeyValuePair<string, string>("@FDATE", Fdate));
-            sqlParameters.Add(new KeyValuePair<string, string>("@ToDate", Tdate));
-            sqlParameters.Add(new KeyValuePair<string, string>("@Godcode", GCode)); 
-             ds = manageSQLConnection.GetDataSetValues("getIssuememoabstract", sqlParameters);
-            return JsonConvert.SerializeObject(ds.Tables[0]);
-        }
+        //[HttpGet("{id}")]
+        //public string Get(string Fdate, string Tdate, string GCode)
+        //{
+        //    DataSet ds = new DataSet();
+        //    ManageSQLConnection manageSQLConnection = new ManageSQLConnection();
+        //    List<KeyValuePair<string, string>> sqlParameters = new List<KeyValuePair<string, string>>();
+        //    sqlParameters.Add(new KeyValuePair<string, string>("@FDATE", Fdate));
+        //    sqlParameters.Add(new KeyValuePair<string, string>("@ToDate", Tdate));
+        //    sqlParameters.Add(new KeyValuePair<string, string>("@Godcode", GCode)); 
+        //     ds = manageSQLConnection.GetDataSetValues("getIssuememoabstract", sqlParameters);
+        //    return JsonConvert.SerializeObject(ds.Tables[0]);
+        //}
 }
 
     public class IssueMemoEntity
@@ -49,5 +62,9 @@ namespace TNCSCAPI.Controllers.Reports.Sales
         public string TCode { get; set; }
         public string Fdate { get; set; }
         public string Tdate { get; set; }
+        public string UserName { get; set; }
+        public string GName { get; set; }
+        public string RName { get; set; }
+        public int Type { get; set; }
     }
 }
