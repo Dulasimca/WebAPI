7b41481c8745cf767555c66a9bb431e31803a1ad
Dulasimca <dulasimca@gmail.com>
2019-10-14 10:34:50 +0530

    Truck transit print changes

--- Controllers/Reports/Transfer/TruckToRegionController.cs
+++ Controllers/Reports/Transfer/TruckToRegionController.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using TNCSCAPI.ManageAllReports;
+using TNCSCAPI.ManageAllReports.Transfer;
 
 namespace TNCSCAPI.Controllers.Reports
 {
@@ -14,7 +16,7 @@ namespace TNCSCAPI.Controllers.Reports
     public class TruckToRegionController : ControllerBase
     {
         [HttpGet("{id}")]
-        public string Get(string FDate, string ToDate, string GCode)
+        public string Get(string FDate, string ToDate, string GCode,string UserName)
         {
             DataSet ds = new DataSet();
             ManageSQLConnection manageSQLConnection = new ManageSQLConnection();
@@ -23,6 +25,22 @@ namespace TNCSCAPI.Controllers.Reports
             sqlParameters.Add(new KeyValuePair<string, string>("@ToDate", ToDate));
             sqlParameters.Add(new KeyValuePair<string, string>("@Godcode", GCode));
             ds = manageSQLConnection.GetDataSetValues("GetTrucktoRegion", sqlParameters);
+            ManageTruckToRegion manageTruckToRegion = new ManageTruckToRegion();
+            ManageReport manageReport = new ManageReport();
+            if (manageReport.CheckDataAvailable(ds))
+            {
+                CommonEntity entity = new CommonEntity
+                {
+                    dataSet = ds,
+                    GCode = GCode,
+                    FromDate = FDate,
+                    Todate = ToDate,
+                    UserName = UserName,
+                    GName = ds.Tables[0].Rows[0]["TNCSName"].ToString(),
+                    RName = ds.Tables[0].Rows[0]["TNRGNAME"].ToString()
+                };
+                Task.Run(() => manageTruckToRegion.GenerateTruckToRegion(entity)); //Generate the Report
+            }
             return JsonConvert.SerializeObject(ds.Tables[0]);
         }
     }
