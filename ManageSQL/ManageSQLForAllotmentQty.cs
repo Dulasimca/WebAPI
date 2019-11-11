7131d8f954c0c2581ff757637b4127ac18735972
chandrika <udhayachandrikam31@gmail.com>
2019-11-11 16:42:29 +0530

    allotment module

---
+++ ManageSQL/ManageSQLForAllotmentQty.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using TNCSCAPI.Controllers.Allotment;
+
+namespace TNCSCAPI.ManageSQL
+{
+    public class ManageSQLForAllotmentQty
+    {
+        SqlConnection sqlConnection = new SqlConnection();
+        SqlCommand sqlCommand = new SqlCommand();
+        public Tuple<bool, string> InsertAllotmentQtyEntry(List<AllotmentQuantityEntity> entity)
+        {
+            SqlTransaction objTrans = null;
+            using (sqlConnection = new SqlConnection(GlobalVariable.ConnectionString))
+            {
+                DataSet ds = new DataSet();
+
+                sqlCommand = new SqlCommand();
+                try
+                {
+                    if (sqlConnection.State == 0)
+                    {
+                        sqlConnection.Open();
+                    }
+                    objTrans = sqlConnection.BeginTransaction();
+                    //sqlCommand.Transaction = objTrans;
+                    //sqlCommand.Connection = sqlConnection;
+                    foreach (var item in entity)
+                    {
+                        foreach (var i in item.ItemList)
+                        {
+                            sqlCommand.Parameters.Clear();
+                            sqlCommand.Dispose();
+
+                            sqlCommand = new SqlCommand();
+                            sqlCommand.Transaction = objTrans;
+                            sqlCommand.Connection = sqlConnection;
+                            sqlCommand.CommandText = "InsertAllotmentQuantity";
+                            sqlCommand.CommandType = CommandType.StoredProcedure;
+                            sqlCommand.Parameters.AddWithValue("@SocietyCode", item.SocietyCode);
+                            sqlCommand.Parameters.AddWithValue("@FPSCode", item.FPSCode);
+                            sqlCommand.Parameters.AddWithValue("@SchemeCode", '-');
+                            sqlCommand.Parameters.AddWithValue("@AMonth", item.AllotmentMonth);
+                            sqlCommand.Parameters.AddWithValue("@AYear", item.AllotmentYear);
+                            sqlCommand.Parameters.AddWithValue("@GCode", item.GCode);
+                            sqlCommand.Parameters.AddWithValue("@Taluk", item.Taluk);
+
+                            sqlCommand.Parameters.AddWithValue("@ITCode", i.ITCode);
+                            sqlCommand.Parameters.AddWithValue("@Quantity", i.Quantity);
+
+                            sqlCommand.ExecuteNonQuery();
+                        }
+                    }
+                    objTrans.Commit();
+                    return new Tuple<bool, string>(true, GlobalVariable.SavedMessage);
+                }
+                catch (Exception ex)
+                {
+                    objTrans.Rollback();
+                    AuditLog.WriteError(ex.Message + " : " + ex.StackTrace);
+                    return new Tuple<bool, string>(false, GlobalVariable.ErrorMessage);
+                }
+                finally
+                {
+                    sqlConnection.Close();
+                    sqlCommand.Dispose();
+                    ds.Dispose();
+                }
+            }
+        }
+    }
+}
