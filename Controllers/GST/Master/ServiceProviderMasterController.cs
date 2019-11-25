529a03ef6a6ac90ea3440f1b7da083ca27cacb16
Your Name <subashvishal64@gmail.com>
2019-11-25 15:31:36 +0530

    GST Service Provider and Service Provider Name Master api included

---
+++ Controllers/GST/Master/ServiceProviderMasterController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using Newtonsoft.Json;
+
+
+namespace TNCSCAPI.Controllers.GST.Master
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ServiceProviderMasterController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            ManageSQLConnection manageSQLConnection = new ManageSQLConnection();
+            DataSet ds = new DataSet();
+            try
+            {
+                ds = manageSQLConnection.GetDataSetValues("GetGSTServiceProviderMaster");
+                return JsonConvert.SerializeObject(ds.Tables[0]);
+            }
+            finally
+            {
+                ds.Dispose();
+            }
+        }
+    }
+}
