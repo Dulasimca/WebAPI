7a9e0d5c45b8e194cf2a446cff3e2c9d23b802aa
Dulasimca <dulasimca@gmail.com>
2019-12-02 16:43:19 +0530

    Changes added

--- ManageReports.cs
+++ ManageReports.cs
@@ -607,6 +607,35 @@ namespace TNCSCAPI
             }
 
         }
+        /// <summary>
+        /// Convert List of items to data table
+        /// </summary>
+        /// <typeparam name="T">Generic List type</typeparam>
+        /// <param name="items">Generic List Items</param>
+        /// <returns>Data table values</returns>
+        public DataTable ListToDataTable<T>(List<T> items)
+        {
+            DataTable dataTable = new DataTable(typeof(T).Name);
+            //Get all the properties by using reflection   
+            PropertyInfo[] Props = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+            foreach (PropertyInfo prop in Props)
+            {
+                //Setting column names as Property names  
+                dataTable.Columns.Add(prop.Name);
+            }
+            foreach (T item in items)
+            {
+                var values = new object[Props.Length];
+                for (int i = 0; i < Props.Length; i++)
+                {
+
+                    values[i] = Props[i].GetValue(item, null);
+                }
+                dataTable.Rows.Add(values);
+            }
+
+            return dataTable;
+        }
 
         /// <summary>
         /// Add more content to display next line.
