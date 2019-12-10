5cb43a681f6475112da4d677f66ccb8ee343ed58
Dulasimca <dulasimca@gmail.com>
2019-12-10 16:47:34 +0530

    Changes added

---
+++ Controllers/Reports/QuantityAccount/QuantityAccountIssuesAndReciptController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TNCSCAPI.Controllers.Reports.QuantityAccount
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuantityAccountIssuesAndReciptController : ControllerBase
+    {
+    }
+}
