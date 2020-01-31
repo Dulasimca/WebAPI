90ec93f7edc8eb2f39118213ef707238d660a2cf
Dulasimca <dulasimca@gmail.com>
2020-01-31 14:33:32 +0530

    gate pass changes

---
+++ ManageSQL/ManageGatePass.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace TNCSCAPI.ManageSQL
+{
+    public class ManageGatePass
+    {
+        SqlConnection sqlConnection = new SqlConnection();
+        SqlCommand sqlCommand = new SqlCommand();
+        public void InsertGatePass(GatePassEntity gatePass)
+        {
+            SqlTransaction objTrans = null;
+            sqlCommand = new SqlCommand();
+            sqlCommand = new SqlCommand();
+            sqlCommand.Transaction = objTrans;
+            sqlCommand.Connection = sqlConnection;
+            sqlCommand.CommandText = "InsertGatePass";
+            sqlCommand.CommandType = CommandType.StoredProcedure;
+            sqlCommand.Parameters.AddWithValue("@GatePassId", "0");
+            sqlCommand.Parameters.AddWithValue("@LorryNumber", gatePass.LorryNumber);
+            sqlCommand.Parameters.AddWithValue("@PrintFlag", "0");
+            sqlCommand.Parameters.AddWithValue("@DocumentTye", gatePass.DocumentTye);
+            sqlCommand.Parameters.AddWithValue("@GCode", gatePass.GCode);
+            sqlCommand.Parameters.AddWithValue("@RCode", gatePass.RCode);
+            sqlCommand.Parameters.AddWithValue("@DocumentNumber", gatePass.DocumentNumber);
+            sqlCommand.ExecuteNonQuery();
+        }
+    }
+
+    public class GatePassEntity
+    {
+        public string LorryNumber { get; set; }
+        public int DocumentTye { get; set; }
+        public string GCode { get; set; }
+        public string RCode { get; set; }
+        public string DocumentNumber { get; set; }
+    }
+}
