1810691b1d7594ae7c48f8dc7588051efe22a7cb
Dulasimca <dulasimca@gmail.com>
2020-02-17 13:52:22 +0530

    GPS changes added

--- Controllers/Documents/StockIssuesAbstractPrintController.cs
+++ Controllers/Documents/StockIssuesAbstractPrintController.cs
@@ -3,6 +3,7 @@ using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Threading.Tasks;
 using TNCSCAPI.ManageAllReports.Document;
 
 namespace TNCSCAPI.Controllers.Documents
@@ -39,6 +40,15 @@ namespace TNCSCAPI.Controllers.Documents
         public bool Put(GatePassCommonEntity entity)
         {
             ManageSQLConnection manageSQLConnection = new ManageSQLConnection();
+            if (entity.Type == 1)
+            {
+                DataSet ds = new DataSet();
+                List<KeyValuePair<string, string>> sqlParameters1 = new List<KeyValuePair<string, string>>();
+                sqlParameters1.Add(new KeyValuePair<string, string>("@DocumentId", entity.DocNumber));
+                ds = manageSQLConnection.GetDataSetValues("GetStockIssuesForAbstractPrint", sqlParameters1);
+                ManageIssuesAbstractPrint issuesAbstractPrint = new ManageIssuesAbstractPrint();
+                Task.Run(() => issuesAbstractPrint.ProcessDataToGPS(ds, entity));
+            }
             List<KeyValuePair<string, string>> sqlParameters = new List<KeyValuePair<string, string>>();
             sqlParameters.Add(new KeyValuePair<string, string>("@GatePassId", entity.GatePassNo));
             sqlParameters.Add(new KeyValuePair<string, string>("@GCode", entity.GCode));
