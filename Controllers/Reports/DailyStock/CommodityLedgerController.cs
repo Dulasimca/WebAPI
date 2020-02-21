cc2d30e17d9b12f1a9a2db8e1a3b9eef448f390f
Dulasimca <dulasimca@gmail.com>
2020-02-21 15:21:16 +0530

    Changes added

--- Controllers/Reports/DailyStock/CommodityLedgerController.cs
+++ Controllers/Reports/DailyStock/CommodityLedgerController.cs
@@ -3,13 +3,18 @@ using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TNCSCAPI.ManageAllReports.StockStatement;
 
-namespace TNCSCAPI.ManageAllReports.StockStatement
+namespace TNCSCAPI.Controllers.Reports.DailyStock
 {
-    public class StockStatementByDate
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommodityLedgerController : ControllerBase
     {
         ManageReport manageReport = new ManageReport();
-        public List<DailyStockDetailsEntity> ProcessStockStatement(StockParameter stockParameter)
+        public List<DailyStockDetailsEntity> ProcessCommodityLedger(StockParameter stockParameter)
         {
             decimal _BookBalanceWeight = 0, _PhysicalBalanceWeight = 0, _CumulitiveShortage = 0, _Shortage = 0;
             decimal _receiptuptoYesterday = 0, _receipttotay = 0, _issuesuptoYesterday = 0,
@@ -159,173 +164,9 @@ namespace TNCSCAPI.ManageAllReports.StockStatement
             {
                 AuditLog.WriteError(" Process Daily Stock " + ex.Message);
             }
-            ManageStockStatement manageStockStatement = new ManageStockStatement();
-            Task.Run(() => manageStockStatement.GenerateStockStatementReport(dailyStockDetailsEntities, stockParameter)); //Generate the Report
+          //  ManageStockStatement manageStockStatement = new ManageStockStatement();
+           // Task.Run(() => manageStockStatement.GenerateStockStatementReport(dailyStockDetailsEntities, stockParameter)); //Generate the Report
             return dailyStockDetailsEntities;
         }
-    }
-    public class DailyStockDetailsEntity
-    {
-        public string ItemCode { get; set; }
-        public string Date { get; set; }
-        public string GName { get; set; }
-        public decimal TNCSCapacity { get; set; }
-        public string RName { get; set; }
-        public string ITDescription { get; set; }
-        public decimal OpeningBalance { get; set; }
-        public decimal PhycialBalance { get; set; }
-        public decimal ClosingBalance { get; set; }
-        public decimal CSBalance { get; set; }
-        public decimal Shortage { get; set; }
-        public decimal TotalReceipt { get; set; }
-        public decimal IssueSales { get; set; }
-        public decimal IssueOthers { get; set; }
-        public DateTime LastUpdated { get; set; }
-        public string GodownCode { get; set; }
-        public string RegionCode { get; set; }
-        public string Remarks { get; set; }
-        public bool Flag { get; set; }
-    }
-
-    public class StockParameter
-    {
-        public string FDate { get; set; }
-        public string ToDate { get; set; }
-        public string GCode { get; set; }
-        public string RCode { get; set; }
-        public string GName { get; set; }
-        public string RName { get; set; }
-        public string UserName { get; set; }
-        public string ItemCode { get; set; }
-    }
-
-    public class StockCBEntity
-    {
-        public StockCBEntity()
-        {
-            RNAME = string.Empty;
-            TNCSName = string.Empty;
-            TNCSCapacity = default(decimal); 
-            GStatus = string.Empty;
-            RStatus = string.Empty;
-            GRemark = string.Empty;
-            RRemark = string.Empty;
-            PADDY_A = default(decimal);
-            PADDY_COMMON = default(decimal);
-            RAW_RICE_COMMON = default(decimal);
-            BOILED_RICE_COMMON = default(decimal);
-            SUGAR = default(decimal);
-            WHEAT = default(decimal);
-            RAVA = default(decimal);
-            MAIDA = default(decimal);
-            TOOR_DHALL = default(decimal);
-            URID_DHALL = default(decimal);
-            MAZOOR__DHALL = default(decimal);
-            GREEN_GRAM_DHALL = default(decimal);
-            GREEN_GRAM = default(decimal);
-            BENGAL_GRAM = default(decimal);
-            PALMOLIEN_OIL = default(decimal);
-            SALT = default(decimal);
-            TEA = default(decimal);
-            KEROSENE = default(decimal);
-            RAW_RICE_A = default(decimal);
-            BOILED_RICE_A = default(decimal);
-            JUTE_TWINE = default(decimal);
-            PALMOLIEN_POUCH = default(decimal);
-            CEMENT_IMPORTED = default(decimal);
-            ATTA = default(decimal);
-            CEMENT_REGULAR = default(decimal);
-            URID_DHALL_SPLIT = default(decimal);
-            Candian_Yellow_lentil_TD = default(decimal);
-            SALT_FF = default(decimal);
-            RAW_RICE_A_HULLING = default(decimal);
-            RAW_RICE_COM_HULLING = default(decimal);
-            YELLOW_LENTAL_US = default(decimal);
-            URAD_SQ = default(decimal);
-            BOILED_RICE_A_HULLING = default(decimal);
-            BOILED_RICE_C_HULLING = default(decimal);
-            LIARD_LENTIL_GREEN = default(decimal);
-            TUR_LEMON = default(decimal);
-            URAD_FAQ = default(decimal);
-            TUR_ARUSHA = default(decimal);
-            URID_DHALL_FAQ = default(decimal);
-            URID_DHALL_SQ = default(decimal);
-            AMMA_SALT_RFFIS = default(decimal);
-            AMMA_SALT_DFS = default(decimal);
-            AMMA_SALT_LSS = default(decimal);
-            AMMA_CEMENT = default(decimal);
-            AMMA_SALT_CIS = default(decimal);
-            OMR_HULLING = default(decimal);
-            BOILED_RICE_FORTIFIED = default(decimal);
-            Fortified_Rice_Kernels = default(decimal);
-            FRK_Blended_Rice = default(decimal);
-
-        }
-
-        public string RNAME { get; set; }
-        public string TNCSName { get; set; }
-        public decimal TNCSCapacity { get; set; }
-        public string GStatus { get; set; }
-        public string RStatus { get; set; }
-        public string GRemark { get; set; }
-        public string RRemark { get; set; }
-        public decimal PADDY_A { get; set; }//IT001
-        public decimal PADDY_COMMON { get; set; }//IT003
-        public decimal RAW_RICE_COMMON { get; set; }//IT009
-        public decimal BOILED_RICE_COMMON { get; set; }//IT012
-        public decimal SUGAR { get; set; }//IT013
-        public decimal WHEAT { get; set; }//IT014
-        public decimal RAVA { get; set; }//IT015
-        public decimal MAIDA { get; set; }//IT016
-        public decimal TOOR_DHALL { get; set; }//IT018
-        public decimal URID_DHALL { get; set; }//IT019
-        public decimal MAZOOR__DHALL { get; set; }//IT020
-        public decimal GREEN_GRAM_DHALL { get; set; }//IT021
-        public decimal GREEN_GRAM { get; set; }//IT024
-        public decimal BENGAL_GRAM { get; set; }//IT025
-        public decimal PALMOLIEN_OIL { get; set; }//IT026
-        public decimal SALT { get; set; }//IT027
-        public decimal TEA { get; set; }//IT029
-        public decimal KEROSENE { get; set; }//IT030
-        public decimal RAW_RICE_A { get; set; }//IT033
-        public decimal BOILED_RICE_A { get; set; }//IT034
-        public decimal JUTE_TWINE { get; set; }//IT041
-        public decimal PALMOLIEN_POUCH { get; set; }//IT059
-        public decimal CEMENT_IMPORTED { get; set; }//IT064
-        public decimal ATTA { get; set; }//IT065
-        public decimal CEMENT_REGULAR { get; set; }//IT066
-        public decimal URID_DHALL_SPLIT { get; set; }//IT112
-        public decimal Candian_Yellow_lentil_TD { get; set; }//IT120
-        public decimal SALT_FF { get; set; }//IT121
-        public decimal RAW_RICE_A_HULLING { get; set; }//IT123
-        public decimal RAW_RICE_COM_HULLING { get; set; }//IT124
-        public decimal YELLOW_LENTAL_US { get; set; }//IT132
-        public decimal URAD_SQ { get; set; }//IT133
-        public decimal BOILED_RICE_A_HULLING { get; set; }//IT139
-        public decimal BOILED_RICE_C_HULLING { get; set; }//IT140
-        public decimal LIARD_LENTIL_GREEN { get; set; }//IT166
-        public decimal TUR_LEMON { get; set; }//IT175
-        public decimal URAD_FAQ { get; set; }//IT176
-        public decimal TUR_ARUSHA { get; set; }//IT190
-        public decimal URID_DHALL_FAQ { get; set; }//IT191
-        public decimal URID_DHALL_SQ { get; set; }//IT192
-        public decimal AMMA_SALT_RFFIS { get; set; }//IT209
-        public decimal AMMA_SALT_DFS { get; set; }//IT210
-        public decimal AMMA_SALT_LSS { get; set; }//IT211
-        public decimal AMMA_CEMENT { get; set; }//IT212
-        public decimal AMMA_SALT_CIS { get; set; }//IT215
-        public decimal OMR_HULLING { get; set; }//IT225
-        public decimal BOILED_RICE_FORTIFIED { get; set; }//IT229
-        public decimal Fortified_Rice_Kernels { get; set; }//IT254
-        public decimal FRK_Blended_Rice { get; set; }//IT255
-        //public decimal 50KGS_O_N_B	{ get; set; }
-        //public decimal 50KGS_S_S	{ get; set; }
-        //public decimal 50KGS_S_W_P	{ get; set; }
-        //public decimal 50KGS_G_R_M	
-        //public decimal 50KGS_U_S
-        //public decimal 50KG_POLYTHENE_GUNNY
-        //    public decimal 100KGS_S_W_P	{ get; set; }
-        //public decimal 100KGS_U_S{ get; set; }
-
     }
 }
