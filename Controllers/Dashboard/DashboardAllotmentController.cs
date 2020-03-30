3106c6dd9d319a76a8058025c5f8ab02d5f60983
chandrika <udhayachandrikam31@gmail.com>
2020-03-30 12:04:55 +0530

    allotment details for dashboard added

---
+++ Controllers/Dashboard/DashboardAllotmentController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using Newtonsoft.Json;
+
+namespace TNCSCAPI.Controllers.Dashboard
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DashboardAllotmentController : ControllerBase
+    {
+        [HttpGet("{id}")]
+        public string Get(string Month, string Year, string GCode)
+        {
+            ManageSQLConnection manageSQLConnection = new ManageSQLConnection();
+            DataSet ds = new DataSet();
+            List<KeyValuePair<string, string>> sqlParameters = new List<KeyValuePair<string, string>>();
+            try
+            {
+                sqlParameters.Add(new KeyValuePair<string, string>("@GCode", GCode));
+                sqlParameters.Add(new KeyValuePair<string, string>("@Month", Month));
+                sqlParameters.Add(new KeyValuePair<string, string>("@Year", Year));
+                ds = manageSQLConnection.GetDataSetValues("GetAllotmetforSociety", sqlParameters);
+                return JsonConvert.SerializeObject(ds);
+            }
+            finally
+            {
+                ds.Dispose();
+            }
+        }
+    }
+}
