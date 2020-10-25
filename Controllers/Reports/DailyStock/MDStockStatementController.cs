7974ab297b1ca2c4e7c12cf6cf526f41b5974fe8
Dulasimca <dulasimca@gmail.com>
2020-10-25 14:47:55 +0530

    Changes added

---
+++ Controllers/Reports/DailyStock/MDStockStatementController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace TNCSCAPI.Controllers.Reports.DailyStock
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MDStockStatementController : ControllerBase
+    {
+        [HttpPost("{id}")]
+        public string Post(StockEntity  stockEntity)
+        {
+            DataSet ds = new DataSet();
+            ManageSQLConnection manageSQLConnection = new ManageSQLConnection();
+            if (stockEntity.DocType == 1)// issues
+            {
+                List<KeyValuePair<string, string>> sqlParameters = new List<KeyValuePair<string, string>>();
+                sqlParameters.Add(new KeyValuePair<string, string>("@Date", stockEntity.Date));
+                sqlParameters.Add(new KeyValuePair<string, string>("@Month", stockEntity.Month));
+                sqlParameters.Add(new KeyValuePair<string, string>("@Year", stockEntity.Year));
+                ds = manageSQLConnection.GetDataSetValues("GetMasterDataForMDStatement", sqlParameters);
+            }
+            return JsonConvert.SerializeObject(ds.Tables[0]);
+        }
+    }
+
+    public class StockEntity
+    {
+        public int DocType { get; set; }
+        public string Month { get; set; }
+        public string Year { get; set; }
+        public string UserName { get; set; }
+        public string Date { get; set; }
+    }
+}
