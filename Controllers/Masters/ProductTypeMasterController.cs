6fa5a57188d58f040434e22fe63abc1d5af9c55b
chandrika <udhayachandrikam31@gmail.com>
2020-12-07 13:34:12 +0530

    quotation details code commit

---
+++ Controllers/Masters/ProductTypeMasterController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace TNCSCAPI.Controllers.Masters
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductTypeMasterController : ControllerBase
+    {
+        SqlConnection sqlConnection = new SqlConnection();
+        SqlCommand sqlCommand = new SqlCommand();
+
+        [HttpGet]
+        public string Get()
+        {
+            DataSet ds = new DataSet();
+            ManageSQLConnection manageSQLConnection = new ManageSQLConnection();
+            ds = manageSQLConnection.GetDataSetValues("GetProductTypeMaster");
+            return JsonConvert.SerializeObject(ds.Tables[0]);
+        }
+
+        [HttpPost("{id}")]
+        public Tuple<bool, string> Post(string PName)
+        {
+            SqlTransaction objTrans = null;
+            using (sqlConnection = new SqlConnection(GlobalVariable.ConnectionString))
+            {
+                DataSet ds = new DataSet();
+
+                sqlCommand = new SqlCommand();
+                try
+                {
+                    if (sqlConnection.State == 0)
+                    {
+                        sqlConnection.Open();
+                    }
+                    objTrans = sqlConnection.BeginTransaction();
+                    sqlCommand.Parameters.Clear();
+                    sqlCommand.Dispose();
+
+                    sqlCommand = new SqlCommand();
+                    sqlCommand.Transaction = objTrans;
+                    sqlCommand.Connection = sqlConnection;
+                    sqlCommand.CommandText = "InsertQuotationDetails";
+                    sqlCommand.CommandType = CommandType.StoredProcedure;
+                    sqlCommand.Parameters.AddWithValue("@PName", PName);
+                    sqlCommand.ExecuteNonQuery();
+
+                    objTrans.Commit();
+                    sqlCommand.Parameters.Clear();
+                    sqlCommand.Dispose();
+                    return new Tuple<bool, string>(true, GlobalVariable.SavedMessage);
+                }
+                catch (Exception ex)
+                {
+                    objTrans.Rollback();
+                    AuditLog.WriteError(ex.Message + " : " + ex.StackTrace);
+                    return new Tuple<bool, string>(false, GlobalVariable.ErrorMessage);
+                }
+                finally
+                {
+                    sqlConnection.Close();
+                    sqlCommand.Dispose();
+                    ds.Dispose();
+                }
+            }
+        }
+    }
+}
---
+++ Models/Documents/QuotationEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TNCSCAPI.Models.Documents
+{
+    public class QuotationEntity
+    {
+            public List<int> ProductID { get; set; }
+            public string GCode { get; set; }
+            public string RCode { get; set; }
+            public string Remarks { get; set; }
+            public string EmailID { get; set; }
+            public string PhoneNo { get; set; }
+    }
+}
