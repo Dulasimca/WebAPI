cadaee06d54abe9946cef90bc58023ef126e263e
chandrika <udhayachandrikam31@gmail.com>
2020-12-13 14:49:43 +0530

    mail sending code commit

---
+++ Models/MailEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TNCSCAPI.Models
+{
+    public class MailEntity
+    {
+        public string FromMailid { get; set; }
+        public string FromPassword { get; set; }
+        public string ToMailid { get; set; }
+        public string ToCC { get; set; }
+        public int Port { get; set; }
+        public string Subject { get; set; }
+        public string BodyMessage { get; set; }
+        public string SMTP { get; set; }
+    }
+}
