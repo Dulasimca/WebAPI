d79eaec1e0d5f38a84d887cceda598370294e40e
Dulasimca <dulasimca@gmail.com>
2020-12-21 14:26:33 +0530

    changes added

--- Controllers/Documents/StockDeliveryOrderController.cs
+++ Controllers/Documents/StockDeliveryOrderController.cs
@@ -1,14 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using TNCSCAPI.Models.Documents;
-using TNCSCAPI.ManageSQL;
-using System.Data;
+﻿using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data;
 using TNCSCAPI.ManageAllReports.Document;
+using TNCSCAPI.ManageSQL;
+using TNCSCAPI.Models.Documents;
 
 namespace TNCSCAPI.Controllers.Documents
 {
@@ -17,13 +14,13 @@ namespace TNCSCAPI.Controllers.Documents
     public class StockDeliveryOrderController : ControllerBase
     {
         [HttpPost("{id}")]
-        public Tuple<bool,string> Post(DocumentDeliveryOrderEntity deliveryOrderEntity = null)
+        public Tuple<bool, string> Post(DocumentDeliveryOrderEntity deliveryOrderEntity = null)
         {
             if (deliveryOrderEntity.Type == 2)
             {
                 ManageDocumentDeliveryOrder documentDO = new ManageDocumentDeliveryOrder();
                 documentDO.GenerateDeliveryOrderText(deliveryOrderEntity);
-                return new Tuple<bool, string>(true,"Print Generated Successfully");
+                return new Tuple<bool, string>(true, "Print Generated Successfully");
             }
             else
             {
@@ -34,18 +31,14 @@ namespace TNCSCAPI.Controllers.Documents
                 ManageReport manageReport = new ManageReport();
                 if (manageReport.CheckDataAvailable(result))
                 {
-                    //if (deliveryOrderEntity.Dono.Trim() != "0")
-                    //{
-                    //    List<KeyValuePair<string, string>> sqlParameters1 = new List<KeyValuePair<string, string>>();
-                    //    sqlParameters1.Add(new KeyValuePair<string, string>("@Type", "4"));
-                    //    sqlParameters1.Add(new KeyValuePair<string, string>("@DocNumber", deliveryOrderEntity.Dono.Trim()));
-                    //    var result1 = manageSQLConnection.GetDataSetValues("CheckDocumentEdit", sqlParameters1);
-                    //    if (!manageReport.CheckDataAvailable(result1))
-                    //    {
-                    //        return new Tuple<bool, string>(false, GlobalVariable.DocumentEditPermission);
-                    //    }
-                    //    // CheckDocumentEdit
-                    //}
+                    //Check data is available in GSTSalesTax table
+                    List<KeyValuePair<string, string>> sqlParameters1 = new List<KeyValuePair<string, string>>();
+                    sqlParameters1.Add(new KeyValuePair<string, string>("@BillNo", deliveryOrderEntity.Dono.Trim()));
+                    var result1 = manageSQLConnection.GetDataSetValues("GetSalesTaxByBillNo", sqlParameters1);
+                    if (manageReport.CheckDataAvailable(result1))
+                    {
+                        return new Tuple<bool, string>(false, GlobalVariable.DocumentEditPermissionForDO);
+                    }
                     ManageDeliveryOrder manageDelivery = new ManageDeliveryOrder();
                     return manageDelivery.InsertDeliveryOrderEntry(deliveryOrderEntity);
                 }
@@ -54,11 +47,11 @@ namespace TNCSCAPI.Controllers.Documents
                     return new Tuple<bool, string>(false, "Permission not Granted");
                 }
             }
-           
+
         }
 
         [HttpGet("{id}")]
-        public string Get(string sValue, int Type,string GCode)
+        public string Get(string sValue, int Type, string GCode)
         {
             DataSet ds = new DataSet();
             ManageSQLConnection manageSQLConnection = new ManageSQLConnection();
