6d9fa2167530361251612c27b84db5137a00e7f9
Dulasimca <dulasimca@gmail.com>
2020-12-22 13:10:59 +0530

    Changes added

--- Models/Documents/QuotationEntity.cs
+++ Models/Documents/QuotationEntity.cs
@@ -1,18 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace TNCSCAPI.Models.Documents
 {
     public class QuotationEntity
     {
-            public List<int> ProductID { get; set; }
-            public string GCode { get; set; }
-            public string RCode { get; set; }
-            public string Remarks { get; set; }
-            public string EmailID { get; set; }
-            public string PhoneNo { get; set; }
-            public string Products { get; set; }
+        public List<int> ProductID { get; set; }
+        public string GCode { get; set; }
+        public string RCode { get; set; }
+        public string Remarks { get; set; }
+        public string EmailID { get; set; }
+        public string PhoneNo { get; set; }
+        public string Products { get; set; }
+        public string GName { get; set; }
+        public string RNname { get; set; }
     }
 }
